4c5413e78f40d82e4acba42f5257485e5358c3c4
webwudinfyre <webwudinfyre@gmail.com>
2024-05-15 18:25:55 +0400

    small  change  profile header

--- resources/views/components/profile-component.blade.php
+++ resources/views/components/profile-component.blade.php
@@ -27,7 +27,7 @@ $data_type=User::find($title);
 
 
          @if ($data->avatar)
-        <img src="{{ asset('storage/images/images/' . $taskCounts) }}" alt="Profile" class="rounded-circle">
+        <img src="{{ asset('storage/images/images/' . $data->avatar) }}" alt="Profile" class="rounded-circle">
     @else
         <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="Profile"
             class="rounded-circle">
