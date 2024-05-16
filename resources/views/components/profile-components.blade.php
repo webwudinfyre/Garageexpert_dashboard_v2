196d4ffe97d2646cd39ac12a61b1752d87234850
webwudinfyre <webwudinfyre@gmail.com>
2024-05-16 11:07:49 +0400

    small  change  profile header

--- resources/views/components/profile-components.blade.php
+++ resources/views/components/profile-components.blade.php
@@ -1,4 +1,4 @@
-<div>123
+<div>
     {{ $taskCounts }}
     {{-- @if ($taskCounts)
     <img src="{{ asset('storage/images/images/' . $data->avatar) }}" alt="Profile" class="rounded-circle">
@@ -6,4 +6,11 @@
     <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="Profile"
         class="rounded-circle">
 @endif --}}
+
+@if ($taskCounts)
+    <img src="{{ asset('storage/images/images/' . $data->avatar) }}" alt="Profile" class="rounded-circle">
+@else
+    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="Profile"
+        class="rounded-circle">
+@endif
 </div>
